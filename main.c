939446f3dc55a98932d0423cdcc3bd1a202ed5ff
Mark Vertenberg <markvertenberg2001@gmail.com>
2021-11-23 18:11:48 +0200

    v0.1

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main(){
+
+    input = ''
+    scanf("%c", &input)
+
+    while(input != 'D'){
+        printf("input: %c", input)
+        scanf("%c", &input)
+    }
     return 0;
 }
--- my_mat.h
+++ my_mat.h
@@ -1,7 +1,12 @@
-#ifndef __MY__LIBRARY__
-#define __MY__LIBRARY__
+#ifndef __MY_LIB__
+#define __MY_LIB__
 
+#define SIZE_OF_MATRIX 10 
 
+int[][] insertToMatrix(char[] values)
 
+int isTherePath(int i, int j)
+
+int fastestPath(int i, int j)
 
 #endif
