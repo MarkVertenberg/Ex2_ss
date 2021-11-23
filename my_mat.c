40f5b98589e24baf6898fd03c1efc2f13f83d8d9
Mark Vertenberg <markvertenberg2001@gmail.com>
2021-11-23 17:56:15 +0200

    added titles of functions

--- my_mat.c
+++ my_mat.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int[][] insertToMatrix(char[] values){
+    return 0;
+}
+
+int isTherePath(int i, int j){
+    return 0;
+}
+
+int fastestPath(int i, int j){
+    return 0;
+}
+
+int main(){
     return 0;
 }
